aadd18bd851dcc7d0aaf81aa3df311c016cf06f3
oomiya karen <krn971138@gmail.com>
2021-02-18 11:04:16 +0900

    課題修正完了

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,6 @@
 class PostMailer < ApplicationMailer
   def post_mail(picture)
     @picture = picture
-    mail to: "krn971138@gmail.com", subject: "投稿確認メール"
+    mail to: @picture.user.email, subject: "投稿確認メール"
   end
 end
