68ecbfc582dd44eb2c059d772b8ec00ca5792513
oomiya karen <krn971138@gmail.com>
2021-03-16 11:30:19 +0900

    メアド削除

---
+++ .history/app/controllers/users_controller_20210308173218.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_signed_in, only: %i[ show edit update destroy ]
+  before_action :check_user_not_edit, only: %i[ show edit update destroy ]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show 
+    @user = User.find(params[:id])
+    # @pictures = @user.pictures
+
+    # favorites = Favorite.where(user_id: current_user.id).pluck(:picture_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+    # @favorite_list = Picture.find(favorites)     # postsテーブルから、お気に入り登録済みのレコードを取得
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :image, :image_cache, :password,
+                                  :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def check_user_not_edit
+    unless @user == current_user
+      redirect_to current_user
+    end
+  end
+
+end
